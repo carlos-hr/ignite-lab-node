c469beb4d4f2c69890a53e450f252524c2fee65e
carlos-hr <carlos.henrique17.chr@gmail.com>
2022-12-13 22:38:27 -0300

    feat: creating notification entity and starting tests

---
+++ notifications-service/src/application/entities/notification.ts
@@ -0,0 +1,49 @@
+import { Replace } from 'src/helpers/Replace';
+import { Content } from './content';
+
+export interface NotificationProps {
+  category: string;
+  content: Content;
+  readAt?: Date | null;
+  createdAt: Date;
+  recipientId: string;
+}
+
+export class Notification {
+  private props: NotificationProps;
+
+  constructor(props: Replace<NotificationProps, { createdAt?: Date }>) {
+    this.props = {
+      ...props,
+      createdAt: props.createdAt ?? new Date(),
+    };
+  }
+
+  public set category(category: string) {
+    this.props.category = category;
+  }
+
+  public get category(): string {
+    return this.props.category;
+  }
+
+  public set content(content: Content) {
+    this.props.content = content;
+  }
+
+  public get content(): Content {
+    return this.props.content;
+  }
+
+  public set readAt(readAt: Date | null | undefined) {
+    this.props.readAt = readAt;
+  }
+
+  public get readAt(): Date | null | undefined {
+    return this.props.readAt;
+  }
+
+  public get createdAt(): Date {
+    return this.props.createdAt;
+  }
+}
